fff084e4a3626bfe0527559e45b6ccd6fb7b3ca9
Yefhem <sametcelikbusiness@gmail.com>
2022-11-18 18:12:07 +0300

    deneme

--- GFNetwork/Package.swift
+++ GFNetwork/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on. // deneme
         .target(
             name: "GFNetwork",
             dependencies: []),
